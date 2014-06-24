5d213a63a1c46a35660bbeaf7eb846c4746c4ee4
balbano <balbano@gmail.com>
2014-06-23 20:09:42 -0500

    Added fps reporting

--- PrairieAveLive_MicNode.ino
+++ PrairieAveLive_MicNode.ino
@@ -1,51 +1,81 @@
-/***********************************************
-Microphone Node, Arduino Fio, Electret mic, XBEE
-************************************************/
+/* 
+Prairie Avenue Live - Interior Microphone Node
+==============================================
+
+The seven microphone nodes are placed throughout the Arts Incubator.
+Each node transmits volume data from the mic to the microcontroller
+running the LED panel.
+
+Parts
+-----
+
+- Arduino Fio: https://www.sparkfun.com/products/10116
+- Electret mic with auto gain: http://www.adafruit.com/products/1713
+- XBee Series 2: http://www.adafruit.com/products/968
+
+*/
 
 #include <XBee.h>
+#include <SoftwareSerial.h>
 
-// SET UP XBEE
+// AUDIO
+const int micPin = 1;
+uint8_t sampleMax;
+const uint8_t sampleFloor = 100; // Use mic_check sketch to determine value.
+const uint8_t sampleCeiling = 190; // Use mic_check sketch to determine value.
+
+// XBEE
 XBee xbee = XBee();
-
 uint8_t payload[] = {0};
-
 XBeeAddress64 addr64 = XBeeAddress64(0x0, 0x0); // coord 0x0013a200, 0x40ace8df
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 
-// SET UP AUDIO
-const int micPin = 1;
-const uint8_t sampleFloor = 100; // Use mic_check sketch to determine value.
-const uint8_t sampleCeiling = 190; // Use mic_check sketch to determine value.
-const int sampleWindow = 40; // 25 FPS. (Panel runs at 12 FPS).
-unsigned long prevMillis;
+// TIMERS
+const int timeBetweenTransmissions = 40; // 25 FPS. (Panel runs at 12 FPS).
+unsigned long prevTransmissionTime;
+
+const int timeBetweenReports = 1000; // Report once per second.
+unsigned long prevReportTime;
+
+// REPORTING
+SoftwareSerial softSerial(4, 5); // RX, TX
+int numberOfTransmissions;
 
 void setup() 
 {
   Serial.begin(9600);
   xbee.setSerial(Serial);
   
-  prevMillis = millis();
+  softSerial.begin(4800);
+  
+  // Initialize the timers and counters;
+  prevTransmissionTime = millis();
+  prevReportTime = millis();
+  numberOfTransmissions = 0;
+  sampleMax = 0;
 }
 
 void loop() 
 {
-  uint8_t sampleMax = 0;
+  uint8_t rawSample = getSample(micPin);
+  if (rawSample > sampleMax) {
+    sampleMax = rawSample;
+  }
   
-  // Always take at least one sample.
-  do {
-    uint8_t rawSample = getSample(micPin);
-    if (rawSample > sampleMax) {
-      sampleMax = rawSample;
-    }
-  } while (millis() - prevMillis < sampleWindow);
+  if (millis() - prevTransmissionTime > timeBetweenTransmissions) {
+    prevTransmissionTime = millis();
+    uint8_t factoredSample = factorSample(sampleMax, sampleFloor, sampleCeiling);
+    payload[0] = factoredSample;
+    xbee.send(zbTx);
+    numberOfTransmissions++;
+    sampleMax = 0;
+  }
   
-  prevMillis = millis();
-  
-  uint8_t factoredSample = factorSample(sampleMax, sampleFloor, sampleCeiling);
-  
-  payload[0] = factoredSample;
-  
-  xbee.send(zbTx);
+  if (millis() - prevReportTime > timeBetweenReports) {
+    prevReportTime = millis();
+    softSerial.println(numberOfTransmissions);
+    numberOfTransmissions = 0;
+  }
 }
 
 uint8_t getSample(int pin) {
