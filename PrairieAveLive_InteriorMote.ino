60fc9d643fd4f89b33e3ab768b682aac7efef032
balbano <balbano@gmail.com>
2014-06-28 11:02:20 -0500

    updated Readme

--- PrairieAveLive_InteriorMote.ino
+++ PrairieAveLive_InteriorMote.ino
@@ -1,17 +1,26 @@
 /* 
-Prairie Avenue Live - Interior Microphone Node
-==============================================
-
-The seven microphone nodes are placed throughout the Arts Incubator.
-Each node transmits volume data from the mic to the microcontroller
-running the LED panel.
-
-Parts
------
-
-- Arduino Fio: https://www.sparkfun.com/products/10116
-- Electret mic with auto gain: http://www.adafruit.com/products/1713
-- XBee Series 2: http://www.adafruit.com/products/968
+  Prairie Avenue Live - Interior Mote
+  ===================================
+  
+  The seven microphone nodes are placed throughout the Arts Incubator.
+  Each node transmits volume data from the mic to the microcontroller
+  running the LED panel.
+  
+  Terminology
+  -----------
+  
+  - Mote: Arduino Fio and XBee collects sound levels from mic and transmits to controller.
+    - There are multiple interior motes with one mic each and one exterior mote with multiple mics.
+  - Mic: the electret mics used by the motes.
+  - Controller: Teensy 3.1 and XBee that receive the sound data from the motes and control the LEDs.
+  - Node: the origin point (on the LED array) of the visualization for a particular mic.
+  
+  Parts
+  -----
+  
+  - Arduino Fio: https://www.sparkfun.com/products/10116
+  - Electret mic with auto gain: http://www.adafruit.com/products/1713
+  - XBee Series 2: http://www.adafruit.com/products/968
 
 */
 
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-Interior mic node!
+Prairie Avenue Live - Interior Mote
+===================================
+
+The seven microphone nodes are placed throughout the Arts Incubator.
+Each node transmits volume data from the mic to the microcontroller
+running the LED panel.
+
+Terminology
+-----------
+
+- Mote: Arduino Fio and XBee collects sound levels from mic and transmits to controller.
+  - There are multiple interior motes with one mic each and one exterior mote with multiple mics.
+- Mic: the electret mics used by the motes.
+- Controller: Teensy 3.1 and XBee that receive the sound data from the motes and control the LEDs.
+- Node: the origin point (on the LED array) of the visualization for a particular mic.
+
+Parts
+-----
+
+- Arduino Fio: https://www.sparkfun.com/products/10116
+- Electret mic with auto gain: http://www.adafruit.com/products/1713
+- XBee Series 2: http://www.adafruit.com/products/968
+
+Repos
+-----
+
+ - [PrairieAveLive_Controller](https://github.com/balbano/PrairieAveLive_Controller)
+ - [PrairieAveLive_InteriorMote](https://github.com/balbano/PrairieAveLive_InteriorMote)
+ - [PrairieAveLive_ExteriorMote](https://github.com/balbano/PrairieAveLive_ExteriorMote)
