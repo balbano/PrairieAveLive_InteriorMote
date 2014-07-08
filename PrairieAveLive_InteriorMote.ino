3a6c65a956943ee6b776132ebbbc749187736089
balbano <balbano@gmail.com>
2014-07-08 15:23:02 -0500

    Rewrite to send data in larger chunks.

--- PrairieAveLive_InteriorMote.ino
+++ PrairieAveLive_InteriorMote.ino
@@ -21,32 +21,41 @@
   - Arduino Fio: https://www.sparkfun.com/products/10116
   - Electret mic with auto gain: http://www.adafruit.com/products/1713
   - XBee Series 2: http://www.adafruit.com/products/968
-
 */
 
 #include <XBee.h>
 #include <SoftwareSerial.h>
 
+// NETWORK PARAMS
+const int numberOfInteriorMotes = 3;
+// Controller alternates between polling an interior mote and the exterior
+// mote. GoL updates each time. Therefore, the number of samples needs to be
+// twice the number of interior motes to provide enough data to last until the
+// next time the interior mote is polled.
+const int samplesPerTx = numberOfInteriorMotes * 2;
+
 // AUDIO
 const int micPin = 1;
-uint8_t sampleMax;
 const uint8_t sampleFloor = 100; // Use mic_check sketch to determine value.
 const uint8_t sampleCeiling = 190; // Use mic_check sketch to determine value.
+uint8_t sampleMax; // Holds the max level during the sampling period.
 
 // XBEE
 XBee xbee = XBee();
-uint8_t payload[] = {0};
-XBeeAddress64 addr64 = XBeeAddress64(0x0, 0x0); // coord 0x0013a200, 0x40ace8df
+uint8_t payload[samplesPerTx];
+XBeeAddress64 addr64 = XBeeAddress64(0x0, 0x0); // Coord's actual address: 0x0013a200, 0x40ace8df
+uint16_t addr16;
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 
 XBeeResponse response = XBeeResponse();
+ZBRxResponse rx = ZBRxResponse();
 
 // TIMERS
-const int timeBetweenTransmissions = 40; // 25 FPS. (Panel runs at 12 FPS).
-unsigned long prevTransmissionTime;
-
-const int timeBetweenReports = 1000; // Report once per second.
+const int timeBetweenReports = 5000; // Report once per second.
 unsigned long prevReportTime;
+
+const int timeBetweenSamples = 100; // 10Hz.
+unsigned long prevSampleTime;
 
 // REPORTING
 SoftwareSerial softSerial(4, 5); // RX, TX
@@ -54,38 +63,51 @@ int numberOfTransmissions;
 
 void setup() 
 {
+  // Set up XBee.
   Serial.begin(57600);
   xbee.setSerial(Serial);
+  zbTx.setFrameId(0); // No ACK.
+
+  for (int i = 0; i < samplesPerTx; i++) {
+    payload[i] = 0;
+  }
   
-  softSerial.begin(57600);
-  
-  // Initialize the timers and counters;
-  prevTransmissionTime = millis();
-  prevReportTime = millis();
-  numberOfTransmissions = 0;
+  // Set up audio.
   sampleMax = 0;
+
+  // Set up reporting.
+  softSerial.begin(57600);
+  numberOfTransmissions = 0;
+
+  // Timers.
+  prevSampleTime = millis();
+  prevReportTime = millis();
 }
 
 void loop() 
 {
   uint8_t rawSample = getSample(micPin);
+
   if (rawSample > sampleMax) {
     sampleMax = rawSample;
   }
-  
-  xbee.readPacket();
-  if (xbee.getResponse().isAvailable()) {
-    //prevTransmissionTime = millis();
-    uint8_t factoredSample = factorSample(sampleMax, sampleFloor, sampleCeiling);
-    payload[0] = factoredSample;
-    xbee.send(zbTx);
-    numberOfTransmissions++;
+
+  if (millis() - prevSampleTime > timeBetweenSamples) {
+    prevSampleTime = millis();
+    // Shift the payload values to the left and append the newest one on the right.
+    memcpy(&payload[0], &payload[1], (samplesPerTx - 1) * sizeof(*payload));
+    payload[samplesPerTx - 1] = factorSample(sampleMax, sampleFloor, sampleCeiling);
     sampleMax = 0;
   }
   
+  if (lookForData(255)) {
+    xbee.send(zbTx);
+    numberOfTransmissions++;
+  }
+
   if (millis() - prevReportTime > timeBetweenReports) {
     prevReportTime = millis();
-    softSerial.println(numberOfTransmissions);
+    printReport();
     numberOfTransmissions = 0;
   }
 }
@@ -99,4 +121,42 @@ uint8_t getSample(int pin) {
 uint8_t factorSample(uint8_t rawSample, uint8_t thresholdLow, uint8_t thresholdHigh) {
   int factoredSample = map(rawSample, thresholdLow, thresholdHigh, 0, 255);
   return uint8_t(constrain(factoredSample, 0, 255));
+}
+
+boolean lookForData(uint8_t expectedData) {
+  // Look for a packet containing the expected data until the buffer is empty.
+  // Clear the buffer if the expected data is found.
+  xbee.readPacket();
+
+  if (xbee.getResponse().isAvailable()) {
+    if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
+      xbee.getResponse().getZBRxResponse(rx);
+      uint8_t data = rx.getData()[0];
+
+      if (data == expectedData) {
+        addr16 = rx.getRemoteAddress16();
+
+        // Clear out the buffer before returning.
+        do {
+          xbee.readPacket();
+        } while (xbee.getResponse().isAvailable() || xbee.getResponse().isError());
+        
+        return true;
+      }
+    } 
+    // If we got a packet, but it wasn't correct, try again in case the packet
+    // we're after is further back in the buffer.
+    return lookForData(expectedData);
+  }
+  if (xbee.getResponse().isError()) {
+    return lookForData(expectedData);
+  }
+  return false; // Did not get the data.
+}
+
+void printReport() {
+  softSerial.print(numberOfTransmissions);
+  softSerial.print(" transmissions in last ");
+  softSerial.print(timeBetweenReports);
+  softSerial.println(" millis.");
 }
